a0d446be48a182de370cea162667107ee85ec78a
Brians Fernando <fervargas2003@hotmail.com>
2023-11-30 22:04:30 -0600

    Revert "Squashed commit of the following:"
    This reverts commit d6740a0883b388b853d54105c11114223c6f4b20, reversing
    changes made to 0fdca3db1068bebf4d7b1def1ce3f13a4471ff95.

--- Proyecto/src/main/java/com/proyecto/dawp/dao/RolDao.java
+++ Proyecto/src/main/java/com/proyecto/dawp/dao/RolDao.java
@@ -1,12 +1,12 @@
 package com.proyecto.dawp.dao;
 
-import com.proyecto.dawp.domain.Perfil;
+import com.proyecto.dawp.domain.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
  * @author ferva
  */
-public interface PerfilDao extends JpaRepository <Perfil, Long> {
-    
+public interface RolDao extends JpaRepository<Rol, Long> {
+
 }
